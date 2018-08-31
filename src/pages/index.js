582b8435694e014ea90f3d7b47d1e1a802eea7bb
Abhilekh Kalita <abhilekh.kalita@gmail.com>
2018-08-31 17:15:52 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to my Gatsby site.</p>
+    <p>My name is Abhilekh Kalita, I am from Guwahati, India.</p>
+    <p>I am here to learn and build something great together.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
